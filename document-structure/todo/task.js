68506416a0121835cd269307656548d2643db157
GorVit <muteer555@gmail.com>
2023-09-12 17:55:42 +0500

    done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,30 @@
+class Todo {
+  constructor () {
+    this.div = document.querySelector('.tasks__list')
+    this.input = document.querySelector('.tasks__input')
+    this.add = document.querySelector('.tasks__add')
+    this.tasks()
+  }
+  tasks() {
+    this.add.addEventListener('click', e => {
+      e.preventDefault()
+      let elem = document.createElement('div')
+      if (this.input.value.trim() !== '') {
+        elem.classList.add('task')
+        elem.innerHTML += `<div class="task__title">
+      ${this.input.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>`
+        this.div.parentNode.insertBefore(elem, this.div.nextSibling)
+        this.input.value = ''
+    }
+      elem.addEventListener('click', e => {
+        //e.preventDefault()
+        //console.log(e.target)
+        elem.remove()
+      })
+    })
+  }
+}
+
+let todo = new Todo()
